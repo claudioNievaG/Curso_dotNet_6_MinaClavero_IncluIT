db9ca3c41d59d57046b16a9a6fa1966a99c0902f
Claudio Nieva <claudionieva.g@gmail.com>
2022-07-18 00:15:32 -0300

    Tarea clase 5

---
+++ Clase 5/Vuelteretas/Vuelteretas/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string nombreIngresado;
+string doContinue = "S";
+
+while (doContinue == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Cual es tu nombre?");
+    nombreIngresado = Console.ReadLine();
+
+    Console.WriteLine($"Hola {nombreIngresado}!");
+
+    Console.WriteLine("Quieres continuar? (S/N)");
+    doContinue = Console.ReadLine().ToUpper();
+}
+
+if (doContinue == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
