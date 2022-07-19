240fae48ee2766cae5aa4b1ea7c5c8492ee9bb37
Claudio Nieva <claudionieva.g@gmail.com>
2022-07-18 23:58:24 -0300

    ejercicio clase 6

---
+++ Clase 6/Clase 6/Clase 6/Program.cs
@@ -0,0 +1,24 @@
+﻿int secretNumbre;
+string selectedNumber;
+int count = 0;
+
+do
+{
+    //Console.Clear();
+    secretNumbre = new Random(DateTime.Now.Millisecond).Next(1, 21);
+    Console.WriteLine("Adivina el numero entre 1 y 20!");
+    selectedNumber = Console.ReadLine().ToString();
+
+    if (secretNumbre < int.Parse(selectedNumber))
+    {
+        Console.WriteLine("el numero es muy grande! intenta de nuevo");
+        count++;
+    }else if(secretNumbre > int.Parse(selectedNumber))
+    {
+        Console.WriteLine("el numero es muy chico! intenta de nuevo");
+        count++;
+    }
+}
+while (int.Parse(selectedNumber) != secretNumbre);
+
+Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: { secretNumbre } Lo has logrado en { count } intentos!!");
