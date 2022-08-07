a4d613a54ab6c4e169dd973553364f5a8279f754
Claudio Nieva <claudionieva.g@gmail.com>
2022-08-07 15:59:14 -0300

    clase 7

---
+++ Clase 7/Clase 7/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] Frutas = new string[5];
+
+Frutas[0] = "banana";
+Frutas[1] = "manzana";
+Frutas[2] = "peras";
+Frutas[3] = "Naranjas";
+Frutas[4] = "Anana";
+
+for (int i = 1; i < Frutas.Length; i++)
+{
+    Console.WriteLine(Frutas[i]);
+}
---
+++ Tarea_clase_7/Tarea_clase_7/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numeros = new int[10];
+int sum = 0;
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"deme el numero {i + 1} de {numeros.Length} para almacenar");
+    int numero = int.Parse(Console.ReadLine());
+    numeros[i] = numero;
+}
+
+foreach (int numero in numeros)
+{
+    sum += numero;
+}
+
+
+Console.WriteLine($"La suma total es {sum}");
+Console.WriteLine($"el numero mayor es {numeros.Max()}");
+Console.WriteLine($"el numero menor es {numeros.Min()}");
+Console.WriteLine($"el promedio es {sum / numeros.Length}");
+Console.WriteLine("Los numeros agregados son: ");
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
